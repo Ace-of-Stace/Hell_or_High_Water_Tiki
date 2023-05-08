84ac4dcce7a1c2b4c97022e588cbf7de08ed578e
Stacie Barbarick <32335801+Ace-of-Stace@users.noreply.github.com>
2023-05-08 16:44:30 -0600

    Update LavaSmolderFade_WS2812B_FINAL.ino
    In this modified version of the code, we've added a new variable called previousMillis that keeps track of the last time the LED strip was updated. In the loop, we use the millis() function to get the current time and check if it has been longer than BOIL_TIME since the last LED update. If it has, we generate the random boiling effect and update the LED strip. If not, we continue looping without doing anything.
    
    This approach allows the program to continue running while waiting for the time delay to pass, so it remains responsive to user input.

--- LavaSmolderFade_WS2812B_FINAL.ino
+++ LavaSmolderFade_WS2812B_FINAL.ino
@@ -1,28 +1,28 @@
-#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoPixel.h>  // Include the Adafruit_NeoPixel library
 
-#define PIN 6
-#define NUM_LEDS 13
-#define NUM_SEGMENTS 6
-#define SEGMENT_SIZE 2
-#define BOIL_TIME 200 // Time between color changes in ms
+#define PIN 6                   // Define the pin number used to control the LED strip
+#define NUM_LEDS 13             // Define the number of LEDs in the strip
+#define NUM_SEGMENTS 6          // Define the number of segments to divide the strip into
+#define SEGMENT_SIZE 2          // Define the size of each segment
+#define BOIL_TIME 200           // Define the time between color changes in ms
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);  // Create a new instance of the Adafruit_NeoPixel class
 
 void setup() {
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.begin();      // Initialize the LED strip
+  strip.show();       // Set all pixels to 'off'
 }
 
 void loop() {
   // Generate random boiling effect with shades of red
-  for (int i = 0; i < NUM_SEGMENTS; i++) {
-    int r = random(255);
-    int g = 0;
+  for (int i = 0; i < NUM_SEGMENTS; i++) {                 // Iterate through each segment of the LED strip
+    int r = random(255);                                   // Generate a random shade of red
+    int g = 0;                                              // Set green and blue values to 0 for a red-only effect
     int b = 0;
-    for (int j = i * SEGMENT_SIZE; j < (i + 1) * SEGMENT_SIZE; j++) {
-      strip.setPixelColor(j, strip.Color(r, g, b)); // Set color of segment
+    for (int j = i * SEGMENT_SIZE; j < (i + 1) * SEGMENT_SIZE; j++) {    // Iterate through each LED in the segment
+      strip.setPixelColor(j, strip.Color(r, g, b));        // Set the color of the LED to the random shade of red
     }
   }
-  strip.show(); // Update LED strip with new colors
-  delay(BOIL_TIME); // Wait for a short period of time before updating again
+  strip.show();         // Update the LED strip with the new colors
+  delay(BOIL_TIME);     // Wait for a short period of time before updating again
 }
